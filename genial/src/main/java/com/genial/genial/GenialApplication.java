52ec76ac7cc740e0b1ece5fa92122d078c8b8b8f
RicardoJun10r <ricardojunior0110@gmail.com>
2022-08-02 19:43:00 -0300

    Iniciando BackEnd do projeto

---
+++ genial/src/main/java/com/genial/genial/GenialApplication.java
@@ -0,0 +1,13 @@
+package com.genial.genial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenialApplication.class, args);
+	}
+
+}
---
+++ genial/src/test/java/com/genial/genial/GenialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genial.genial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
