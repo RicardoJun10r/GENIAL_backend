3c5bc45a7a40f1505e9c26c61edd450cbe957e67
vitordbo <vitorduarte177@gmail.com>
2022-10-27 15:32:47 -0300

    uuid and Products improvements

--- genial/src/main/java/com/genial/demo/DTO/UserCreateDto.java
+++ genial/src/main/java/com/genial/demo/DTO/UserCreateDto.java
@@ -1,6 +1,9 @@
 package com.genial.demo.DTO;
 
+import java.util.Random;
 import java.util.UUID;
+
+import com.genial.demo.entity.User;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,5 +16,9 @@ public class UserCreateDto {
     private UUID uuid;
     private String name;
     private String password;
+
+    UserCreateDto(){
+        uuid = UUID.randomUUID();
+    }
 
 }
--- genial/src/main/java/com/genial/demo/entity/Product.java
+++ genial/src/main/java/com/genial/demo/entity/Product.java
@@ -1,5 +1,8 @@
 package com.genial.demo.entity;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -30,5 +33,14 @@ public class Product {
     private String description;
     private String sector;
     private Float value;
+    private Date date;  // data que o produto foi cadatrado 
+    private int quantidade; // quantidade de produto no estoque
+
+    // gerar data automaticamente cada vez que se cadastra um produto
+    public Product(){
+        GregorianCalendar gc=new GregorianCalendar(); 
+        date =gc.getTime(); //"Pega" a data do GregorianCalendar para uma variável Date
+        gc.setTime(date); //Armazena a data de d1 para o GregorianCalendar gc.
+    }
     
 }
