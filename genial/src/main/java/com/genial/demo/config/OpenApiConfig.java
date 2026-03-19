16a52f8c0c39acc5f60781ffb8df29f178688f20
RicardoJun10r <ricardojunior0110@gmail.com>
2026-03-18 23:33:59 -0300

    feat: Swagger para documentar api

---
+++ genial/src/main/java/com/genial/demo/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.genial.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI genialOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Genial Inventory API")
+                        .description("API REST para gestao de estoque multitenant com autenticacao JWT.")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
