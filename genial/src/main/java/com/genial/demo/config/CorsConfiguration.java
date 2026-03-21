1176afd4119536f240abd6af5319fb2ca2b62be7
RicardoJun10r <ricardojunior0110@gmail.com>
2026-03-21 13:14:04 -0300

    att

--- genial/src/main/java/com/genial/demo/config/CorsConfiguration.java
+++ genial/src/main/java/com/genial/demo/config/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST",
-                        "PUT", "DELETE");
+                        "PUT", "DELETE").allowedHeaders("*");
             }
         };
     }
--- genial/src/main/java/com/genial/demo/config/SecurityConfig.java
+++ genial/src/main/java/com/genial/demo/config/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -25,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(final HttpSecurity http)
             throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(
